46f29daec74dec3a3cddb95d63aef952b5e8e90c
Christian Fei <info@christian-fei.com>
2013-12-02 14:04:37 +0100

    fddf

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '====stopping cube-phone-controller===='
+forever stop /root/nodestuff/cube-phone-controller/index.js
+echo '====checking out===='
+git checkout .
+echo '====pulling latest changes===='
+git pull
+echo '====npm install===='
+npm install
+echo '====starting forever daemon===='
+HTTP_HOST=37.139.20.20 HTTP_PORT=3007 forever start /root/nodestuff/cube-phone-controller/index.js  >> /root/nodestuff/cube-phone-controller/log.txt 2>&1
